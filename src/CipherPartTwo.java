4b6ffcc2d7f3292c8827b14faf5a32602728edf5
ccapetola20 <42975602+ccapetola20@users.noreply.github.com>
2019-04-22 12:43:27 -0400

    Add files via upload

---
+++ src/CipherPartTwo.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+
+public class CipherPartTwo 
+{
+
+	public static String caesar_cipher(String fileName, boolean encrypt, int shiftAmount) throws IOException
+	{
+		
+
+	}
+	
+	public static boolean isLower(char c)
+	{
+		return c >= 'a' && c <= 'z';
+	}
+	
+	public static boolean isUpper(char c)
+	{
+		return c >= 'A' && c <= 'Z';
+	}
+
+}
