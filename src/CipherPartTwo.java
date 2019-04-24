04c29cb2b1ad005cde199ae421582ee2b96838f2
ccapetola20 <42975602+ccapetola20@users.noreply.github.com>
2019-04-24 12:42:58 -0400

    Add files via upload

--- src/CipherPartTwo.java
+++ src/CipherPartTwo.java
@@ -1,10 +1,105 @@
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class CipherPartTwo 
 {
 
 	public static String caesar_cipher(String fileName, boolean encrypt, int shiftAmount) throws IOException
 	{
+		char[] alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h','i','j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+		char[] upperAlphabet = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+		char[] lowerCipher = new char[26];
+		char[] upperCipher = new char[26];
+		int shift = shiftAmount;
+		int diff = 0;
+		
+		if(encrypt)
+		{
+			for(int i = 0; i<lowerCipher.length;i++)
+			{
+				diff = alphabet[i] - 'a';
+				if((shift + diff) % 26 < 0)
+					lowerCipher[i] = (char) ('z' + (shift + diff + 1));
+				else
+					lowerCipher[i] = (char) (alphabet[i] + shift);
+				
+			}
+			
+			for(int j = 0; j<upperCipher.length; j++)
+				upperCipher[j] = (char) (lowerCipher[j] - 32);
+			
+			File myFile = new File(fileName);
+			Scanner inputFile = new Scanner(myFile);
+			PrintWriter outputFile = new PrintWriter(fileName.substring(0, fileName.length()-4) + "_ENC.txt");
+			
+			String mess = "";
+			String messEnc = "";
+			while(inputFile.hasNext())
+			{
+				mess += inputFile.nextLine();
+				mess += "\n";
+				
+			}
+			
+			int index = 0;
+			for(int i=0; i<mess.length(); i++)
+			{
+				index = 0;
+				if(isUpper(mess.charAt(i)))
+				{
+					while(mess.charAt(i) != upperAlphabet[index])
+						index++;
+					messEnc += upperCipher[index];
+				}
+				else if(isLower(mess.charAt(i)))
+				{
+					while(mess.charAt(i) != alphabet[index])
+						index++;
+					messEnc += lowerCipher[index];
+				}
+				else
+					messEnc += mess.charAt(i);
+			}
+			
+			outputFile.print(messEnc);
+			outputFile.close();
+			inputFile.close();
+			String re = "Result written to " + fileName.substring(0,fileName.length()-4) + "_ENC.txt";
+			return re;
+		}
+		else
+		{
+			for(int i = 0; i<lowerCipher.length;i++)
+			{
+				diff = alphabet[i] - 'a';
+				if((shift + diff) % 26 < 0)
+					lowerCipher[i] = (char) ('z' + (shift + diff + 1));
+				else
+					lowerCipher[i] = (char) (alphabet[i] + shift);
+				
+			}
+			
+			for(int j = 0; j<upperCipher.length; j++)
+				upperCipher[j] = (char) (lowerCipher[j] - 32);
+			
+			File myFile = new File(fileName);
+			Scanner inputFile = new Scanner(myFile);
+			PrintWriter outputFile = new PrintWriter(fileName.substring(0, fileName.length()-4) + "_ENC.txt");
+			
+			String encMess = "";
+			String decMess = "";
+			while(inputFile.hasNext())
+			{
+				encMess += inputFile.nextLine();
+				encMess += "\n";
+				
+			}
+			
+			
+			
+		}
 		
 
 	}
